0f4334a04fbbee5264c0208d2ce75b586b9f1bad
Liza Varela Solórzano <38026679+TSACR-AdminLisa@users.noreply.github.com>
2022-06-29 16:27:57 -0600

    Add files via upload

---
+++ TestMauiApp/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TestMauiApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+
+		Routing.RegisterRoute(nameof(DetailPage), typeof(DetailPage));
+	}
+}
