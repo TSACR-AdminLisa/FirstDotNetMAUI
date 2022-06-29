18ff5f33df66794ee9406e5bbb396b807100e640
Liza Varela Solórzano <38026679+TSACR-AdminLisa@users.noreply.github.com>
2022-06-29 16:30:30 -0600

    Add files via upload

---
+++ TestMauiApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace TestMauiApp.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    IConnectivity connectivity;
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+    
+    [ObservableProperty]
+    string text;
+
+    public MainViewModel(IConnectivity connectivity)
+    {
+        Items = new ObservableCollection<string>();
+        this.connectivity = connectivity;
+    }
+
+    [RelayCommand]
+    async Task Add()
+    { 
+        if (string.IsNullOrWhiteSpace(Text))
+            return;
+
+        if (connectivity.NetworkAccess != NetworkAccess.Internet)
+        {
+            await Shell.Current.DisplayAlert("Uh Oh!", "No Internet", "OK");
+            return;
+        }
+
+        Items.Add(Text);
+
+        Text = string.Empty;
+    }
+
+    [RelayCommand]
+    void Delete(string itemValue)
+    {
+        if (!string.IsNullOrWhiteSpace(itemValue))
+        { 
+            Items.Remove(itemValue);
+        }
+    }
+
+    [RelayCommand]
+    async Task Tap(string itemValue)
+    {
+        if (!string.IsNullOrWhiteSpace(itemValue))
+        {
+            await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Value={itemValue}");
+        }
+    }
+}
